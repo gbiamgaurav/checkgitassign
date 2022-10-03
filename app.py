01661a75a0c83be2a9101d524b3117530b40c053
gbgaurav10 <gaurav.bhattacharya10@outlook.com>
2022-10-03 12:29:16 +0530

    new pull request

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 print('test app.py')
-=======
 print('Helloworld')
 print('my name is {}')
 print('im practising git')
 print('session goingon')
->>>>>>> c37591bb51b82c988f329c4e9736e6f9e48c808b
+print('new pull request')
+
